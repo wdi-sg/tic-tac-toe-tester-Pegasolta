36ee5a7d09cd3cfb051351becc9a2515ca11b3c5
Felix Tan <felixtanhm@gmail.com>
2017-03-19 11:36:45 +0800

    Draw now requires 9 turns instead of 8

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -7,7 +7,7 @@
 // Variable Storage
 var movesMade = []
 var gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-var player = " "
+var player = "playerOne"
 var turn = 1
 // var restart =
 
@@ -43,7 +43,7 @@ function isGameOver () {
     whoWon()
     return true
     }
-  else if (movesMade.length === 8) {
+  else if (movesMade.length === 9) {
     whoWon()
     player = "draw"
     return true
