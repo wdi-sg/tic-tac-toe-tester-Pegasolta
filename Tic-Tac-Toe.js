c7369ca479cb3e9c9a7983e0c54fcc978d344104
Felix Tan <felixtanhm@gmail.com>
2017-03-23 13:30:20 +0800

    added button for restart

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -4,6 +4,9 @@ var gameBoard = [ 0 , 1, 2, 3, 4, 5, 6, 7, 8]
 var player = "playerOne"
 var turn = 1
 
+var $button = $("button")
+$button.on("click", restart())
+
 function playerRotate () {
   if (turn%2 === 0) {
     player = "playerTwo"
